930115478ba4d7aa2a5a2025b92705b5336d9a7f
Alex Brooke <akb.sbc@gmail.com>
2020-09-12 16:34:36 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
+void
+on_center_button()
+{
 	static bool pressed = false;
 	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+	if (pressed) { pros::lcd::set_text(2, "I was pressed!");
 	} else {
 		pros::lcd::clear_line(2);
 	}
