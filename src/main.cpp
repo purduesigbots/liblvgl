096849cd2233b3264cfc39a311fc17901c755ab8
Alex Brooke <akb.sbc@gmail.com>
2020-09-12 16:50:15 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ on_center_button()
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
+		pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
