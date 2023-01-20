868e335528f91895d1c4b7d09fb6623443b64779
ryan-doan <82603138+ryan-doan@users.noreply.github.com>
2023-01-20 14:59:38 -0500

    ⬆️ Update LVGL to 8.3 and LLEMU to function in LVGL 8.3 (#20)
    * Update include/display/lv_conf.h
    
    * Update LVGL to 8.3
    
    * Minor fixes to display.c and llemu.c
    
    * More fixes to llemu.c
    
    * Fix llemu.c
    
    * Change all dynamic file paths to static file paths
    
    * Replace vex calls in lvgl_display_flush and lvgl_read_touch to pros calls
    
    * Fix makefile
    
    * Fix lvgl initialization
    
    * Fix typo
    
    * Temporarily disable LLEMU to fix compilation errors.
    
    * Fix warnings due to mismatching type being passed to functions
    
    * Fix resolution issue.
    
    * Add comments, fix up defs.
    
    * Save progress.
    
    * Fix lvgl input driver initialization
    
    * Rewrite code to assign callbacks to buttons
    
    * Fix the touch driver v2
    
    * Disable scrollbar, changes to padding and centering the buttons
    
    * Fix lcd::print() and example file
    
    * Fix version
    
    * Fix C++ version
    
    Co-authored-by: Jacob Walter <walte250@purdue.edu>
    Co-authored-by: R11G <richlgame31@gmail.com>
    Co-authored-by: Richard Stump <rnstump@purdue.edu>
    Co-authored-by: Will Xu <xu1321@purdue.edu>

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,13 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
+	
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
+
+	
 }
 
 /**
@@ -80,8 +83,9 @@ void opcontrol() {
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		                (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
