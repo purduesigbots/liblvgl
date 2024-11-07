0dec1aeaf27fbcb0edd7cf430bf7a105394b4978
thiccaxe <51303986+thiccaxe@users.noreply.github.com>
2024-11-07 18:26:25 -0500

    update version to 8.3.9
    lvgl is still on 8.3.8

--- include/liblvgl/lv_version.h
+++ include/liblvgl/lv_version.h
@@ -14,9 +14,9 @@ extern "C" {
  *      INCLUDES
  *********************/
 /*Current version of LittlevGL*/
-#define LVGL_VERSION_MAJOR   5
+#define LVGL_VERSION_MAJOR   8
 #define LVGL_VERSION_MINOR   3
-#define LVGL_VERSION_PATCH   0
+#define LVGL_VERSION_PATCH   8
 #define LVGL_VERSION_INFO    ""
 
 
